a1b3b737d6a92faafcba19fedbc70be5eb8f1169
alfei13 <724096472@qq.com>
2017-10-27 17:22:43 +0800

    更新viewcontroller

--- AFSecondView/AFSecondView/ViewController.m
+++ AFSecondView/AFSecondView/ViewController.m
@@ -17,7 +17,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-
+ /*   zhengfeifeideMacBook-Pro:~ zhengfeifei$ cd /Users/zhengfeifei/Desktop/Future
+    zhengfeifeideMacBook-Pro:Future zhengfeifei$ echo "# AFSecondView" >> README.md
+    zhengfeifeideMacBook-Pro:Future zhengfeifei$ git init
+    Initialized empty Git repository in /Users/zhengfeifei/Desktop/Future/.git/
+    zhengfeifeideMacBook-Pro:Future zhengfeifei$ git add README.md
+    zhengfeifeideMacBook-Pro:Future zhengfeifei$ git commit -m "second commit"
+    [master (root-commit) fcd2d0a] second commit
+    1 file changed, 1 insertion(+)
+    create mode 100644 README.md
+    zhengfeifeideMacBook-Pro:Future zhengfeifei$ git remote add origin https://github.com/alfei13/AFSecondView.git
+    zhengfeifeideMacBook-Pro:Future zhengfeifei$ git push -u origin master
+    Counting objects: 3, done.
+    Writing objects: 100% (3/3), 220 bytes | 220.00 KiB/s, done.
+    Total 3 (delta 0), reused 0 (delta 0)
+    To https://github.com/alfei13/AFSecondView.git
+    * [new branch]      master -> master
+    Branch master set up to track remote branch master from origin.
+    zhengfeifeideMacBook-Pro:Future zhengfeifei$ git add /Users/zhengfeifei/Desktop/Future/AFSecondView
+    zhengfeifeideMacBook-Pro:Future zhengfeifei$ git commit
+*/
+    UIView *view = [[UIView alloc] init];
+    [self.view addSubview:view];
 }
 
 
